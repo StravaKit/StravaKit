5220744be316aba09a7a58bc8c89e6671922ce9e
brennanMKE <brennan@smallsharptools.com>
2016-08-23 20:42:37 -0700

    updates travis scripts

--- ci.sh
+++ ci.sh
@@ -4,4 +4,6 @@ set -e
 
 # xctool -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test -test-sdk iphonesimulator9.3
 
-xcodebuild -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test 
+# xcodebuild -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test 
+
+xcodebuild -project StravaKit.xcodeproj -scheme StravaDemo -configuration Debug -sdk iphonesimulator9.3 -destination "platform=iOS Simulator,name=iPhone 6" test -enableCodeCoverage YES
