f4e7347196770cf511d5fbe50436532b4177b515
brennanMKE <brennan@smallsharptools.com>
2016-08-23 20:05:22 -0700

    removes brew and xctool from ci scripts

--- ci.sh
+++ ci.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-xctool -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test -test-sdk iphonesimulator9.3
+# xctool -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test -test-sdk iphonesimulator9.3
+
+xcodebuild -project StravaKit.xcodeproj -scheme StravaDemo -destination "platform=iOS Simulator,name=iPhone 6" test 
